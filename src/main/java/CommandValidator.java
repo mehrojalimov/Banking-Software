11ceb327318d594f895ae3b3588ff378dc64e021
mehrojalimov <alimovmekhroj01@gmail.com>
2023-11-06 02:10:33 -0500

    Two test cases: creating account without account type & creating account with wrong set of commands are invalid

--- src/main/java/CommandValidator.java
+++ src/main/java/CommandValidator.java
@@ -11,8 +11,7 @@ public class CommandValidator {
 		String accountCommand = parts[0].toLowerCase();
 
 		if ("create".equals(accountCommand)) {
-			if (parts.length >= 4 && !bank.accountExistsByUniqueID(Integer.parseInt(parts[2]))
-					&& hasValidUniquesIdAndApr(parts[2], parts[3])) {
+			if (parts.length >= 4 && !doesAccountExist(parts[2]) && hasValidUniquesIdAndApr(parts[2], parts[3])) {
 				if ("Saving".equals(parts[1]) || "Checking".equals(parts[1]) && parts.length == 4) {
 					return true;
 				} else if ("CD".equals(parts[1]) && parts.length == 5 && isInAmountRange(parts[4])) {
@@ -30,12 +29,16 @@ public class CommandValidator {
 	}
 
 	private boolean hasValidUniquesIdAndApr(String uniqueId, String apr) {
-		boolean validUniqueId = uniqueId.matches("^\\d{8}$");
+		try {
+			boolean validUniqueId = uniqueId.matches("^\\d{8}$");
+			double aprValue = Double.parseDouble(apr);
 
-		double aprValue = Double.parseDouble(apr);
-		boolean validApr = aprValue >= 0 && aprValue <= 10;
+			boolean validApr = aprValue >= 0 && aprValue <= 10;
 
-		return validUniqueId && validApr;
+			return validUniqueId && validApr;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 
 	private boolean isInAmountRange(String amount) {
@@ -44,6 +47,15 @@ public class CommandValidator {
 		boolean correctRange = CDAmount >= 1000 && CDAmount <= 10000;
 
 		return correctRange;
+	}
+
+	private boolean doesAccountExist(String uniqueId) {
+		try {
+			int accountId = Integer.parseInt(uniqueId);
+			return bank.accountExistsByUniqueID(accountId);
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 
 }
