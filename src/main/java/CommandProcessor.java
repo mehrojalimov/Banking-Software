4b53fef561f078a8b0da063e69961b545172aa6a
mehrojalimov <alimovmekhroj01@gmail.com>
2023-11-17 22:55:44 -0500

    Refactoring The command processor

--- src/main/java/CommandProcessor.java
+++ src/main/java/CommandProcessor.java
@@ -12,37 +12,11 @@ public class CommandProcessor {
 
 		switch (commandType) {
 		case "create":
-			processCreateCommand(parts);
+			new CreateCommandProcessor(bank).processCommand(parts);
 			break;
 		case "deposit":
-			processDepositCommand(parts);
+			new DepositCommandProcessor(bank).processCommand(parts);
 			break;
 		}
-	}
-
-	private void processCreateCommand(String[] parts) {
-		String accountType = parts[1].toLowerCase();
-		int uniqueId = Integer.parseInt(parts[2]);
-		double apr = Double.parseDouble(parts[3]);
-
-		switch (accountType) {
-		case "checking":
-			bank.addCheckingAccount(uniqueId, apr);
-			break;
-		case "cd":
-			double amount = Double.parseDouble(parts[4]);
-			bank.addCDAccount(uniqueId, apr, amount);
-			break;
-		case "saving":
-			bank.addSavingAccount(uniqueId, apr);
-			break;
-		}
-	}
-
-	private void processDepositCommand(String[] parts) {
-		int uniqueId = Integer.parseInt(parts[1]);
-		double amount = Double.parseDouble(parts[2]);
-
-		bank.deposit(uniqueId, amount);
 	}
 }
--- src/test/java/MasterControlTest.java
+++ src/test/java/MasterControlTest.java
@@ -12,7 +12,7 @@ public class MasterControlTest {
 	List<String> input;
 
 	private void assertSingleCommand(String command, List<String> actual) {
-		assertEquals(1, input.size());
+		assertEquals(1, actual.size());
 		assertEquals(command, actual.get(0));
 	}
 
@@ -52,13 +52,14 @@ public class MasterControlTest {
 		assertEquals("creat checking 12345678 1.0", actual.get(0));
 		assertEquals("depositt 12345678 100", actual.get(1));
 	}
-	/*
-	 * @Test void invalid_to_create_account_with_the_same_id() {
-	 * input.add("create checking 12345678 1.0");
-	 * input.add("create checking 12345678 1.0");
-	 * 
-	 * List<String> actual = masterControl.start(input);
-	 * 
-	 * assertSingleCommand("create checking 12345678 1.0", actual); }
-	 */
+
+	@Test
+	void invalid_to_create_account_with_the_same_id() {
+		input.add("create checking 12345678 1.0");
+		input.add("create checking 12345678 1.0");
+
+		List<String> actual = masterControl.start(input);
+		assertSingleCommand("create checking 12345678 1.0", actual);
+	}
+
 }
