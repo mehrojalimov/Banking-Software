1a47d2d6f5042f2c513eed31307b7b720f279d3a
Mehroj <ma3947@drexel.edu>
2023-11-17 23:02:32 -0500

    Refactoring Command Validator and adding new child classes!!

---
+++ src/main/java/DepositCommandValidator.java
@@ -0,0 +1,47 @@
+public class DepositCommandValidator extends CommandValidationProcessor {
+
+	public DepositCommandValidator(Bank bank) {
+		super(bank);
+	}
+
+	@Override
+	public boolean validateCommand(String[] parts) {
+		return parts.length == 3 && bank.accountExistsByUniqueID(getAccountId(parts[1]))
+				&& isInDepositLimit(parts[1], parts[2]);
+	}
+
+	private int getAccountId(String uniqueId) {
+		return Integer.parseInt(uniqueId);
+	}
+
+	private boolean isInDepositLimit(String uniqueId, String depositAmountStr) {
+		try {
+			int accountId = getAccountId(uniqueId);
+			Account account = bank.retrieveAccount(accountId);
+
+			if (account != null) {
+				String accountType = account.getAccountType().toLowerCase();
+				double depositAmount = Double.parseDouble(depositAmountStr);
+
+				double depositLimit = getDepositLimitForAccountType(accountType);
+
+				return depositAmount <= depositLimit && depositAmount >= 0;
+			}
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
+		return false;
+	}
+
+	private double getDepositLimitForAccountType(String accountType) {
+		switch (accountType) {
+		case "saving":
+			return 2500;
+		case "checking":
+			return 1000;
+		default:
+			return -1;
+		}
+	}
+}
